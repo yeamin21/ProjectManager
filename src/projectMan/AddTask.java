4446cc4c51ede20f4ab2b76b8ab07a68c9482c64
yeamin21 <yeamin21@outlook.com>
2020-01-29 21:23:42 +0600

    update

--- src/projectMan/AddTask.java
+++ src/projectMan/AddTask.java
@@ -6,10 +6,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AddTask {
-    ArrayList<String>task;
+
+    ArrayList<TextField> TxT = new ArrayList<>();
+    Connection con = ConnectDB.connect();
+    PreparedStatement statement;
+
     int i = 0;
     @FXML
     Button btn_done;
@@ -20,22 +27,30 @@ public class AddTask {
 
     @FXML
     void setBtn_addMoreTask() {
-
-        if(i!=5){
-
-            TextField t=new TextField();
-            t.setMaxSize(216,32);
-            gridpane_tasks.add(t,0,i);
+        TextField t = new TextField();
+        if (i != 5) {
+            gridpane_tasks.add(t, 0, i);
             gridpane_tasks.setPadding(new Insets(10, 10, 10, 10));
             i++;
+
         }
+        TxT.add(t);
     }
 
     @FXML
-    void setBtn_done(){
-        for(i=0;i<5;i++)
-        {
-            System.out.println(task.get(i));
+    void setBtn_done() {
+        int s = 0;
+
+        for (TextField x : TxT) {
+            String sql = "insert into projectman.tasks (code,project_code,status) values(" + s + "," + s + ",'ssd')";
+            try {
+                statement = con.prepareStatement(sql);
+                statement.execute();
+                s++;
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+
         }
     }
 }
--- src/projectMan/ArrayTask.java
+++ src/projectMan/ArrayTask.java
@@ -1,5 +1,22 @@
 package projectMan;
 
 public class ArrayTask {
+private int id;
+String task_name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
 }
