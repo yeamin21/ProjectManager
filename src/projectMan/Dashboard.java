24f62d33e9a0f7d224f3196d5c0fdc3750e2e3de
yeami <yeamin21@outlook.com>
2020-02-05 20:45:27 +0600

    update

--- src/projectMan/Dashboard.java
+++ src/projectMan/Dashboard.java
@@ -3,49 +3,82 @@ package projectMan;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class Dashboard extends Application {
-  @FXML
-  Button btn_b1;
-  @FXML
-  GridPane gridpane_p1;
-  @FXML
-  AnchorPane anc_pan_addnewproject;
+public class Dashboard extends Application implements Initializable {
+    @FXML
+    Button btn_b1;
+    @FXML
+    GridPane gridpane_p1;
     @FXML
     AnchorPane borderPane_p1;
     @FXML
     Pane ppp1;
+    int i = 1;
+    ArrayList<Projects> p = new ArrayList<>();
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    void getProjects() {
+        String sql = "select * from projects";
+        Connection con = ConnectDB.connect();
+        try {
+            ResultSet rs = con.prepareStatement(sql).executeQuery();
+            while (rs.next()) {
+
+                Projects x = new Projects();
+                x.setProject_name(rs.getString("project_name"));
+                p.add(x);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+    @FXML
+    void AddNewProjectPanel() {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("AddNewProject.fxml"));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        ppp1.getChildren().setAll(root);
+
   }
-
   @FXML
-  void AddNewProjectPanel() {
+  void xx() {
       Parent root = null;
       try {
-          root = FXMLLoader.load(getClass().getResource("AddNewProject.fxml"));
-      } catch (Exception e) {
-          System.out.println(e);
+          root = FXMLLoader.load(getClass().getResource("projectInfo.fxml"));
+      } catch (IOException e) {
+          e.printStackTrace();
       }
-      ppp1.getChildren().setAll(root);
+      gridpane_p1.add(root, 0, 0);
+  }
 
-  }
-  @FXML
-  void xx()
-  {
-    Parent root=null;
-    try {
-      root = FXMLLoader.load(getClass().getResource("projectInfo.fxml"));
-    } catch (IOException e) {
-      e.printStackTrace();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        getProjects();
+        gridpane_p1.addRow(2);
+        gridpane_p1.add(new Text("XXP"), 0, 2);
     }
-    gridpane_p1.add(root,0,0);
-  }
 }
---
+++ src/projectMan/Projects.java
@@ -0,0 +1,22 @@
+package projectMan;
+
+public class Projects {
+    private String project_name;
+    private int project_id;
+
+    public String getProject_name() {
+        return project_name;
+    }
+
+    public void setProject_name(String project_name) {
+        this.project_name = project_name;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+}
